a1f625143d1fac1918464cb9ad01626227d90249
Jazmin <ACER@LAPTOP-FRMPB979>
2025-05-21 14:02:52 -0600

    operaciones

---
+++ operacion.java
@@ -0,0 +1,17 @@
+public class operacion {
+      public int sumar(int a, int b) {
+       if (a<0 || b<0) {
+          System.out.println("No se puede sumar numeros negativos");
+          return 0;
+           
+       }
+
+          return a + b;
+      }
+      public double sumar(double a, double b) {
+          return a + b;
+      }
+      public int sumar(int a, int b, int c) {
+          return a + b + c;
+      }
+}
